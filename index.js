5a437d3ae986d98f6736650fe0e5184425439fa1
SeoHyun Ahn <ahn67@purdue.edu>
2018-05-14 13:18:42 -0400

    hw1 change text when button clicked

---
+++ index.js
@@ -0,0 +1,8 @@
+//console.log('ot works');
+//hw: make button change text in the heading
+function changeh1(){
+  const header = document.querySelector('h1')
+  header.textContent = "changed"
+}
+// add multiple heading to the page and make the button change the second one
+//add a form to the page, text input in form, update the heading with the text typeed
