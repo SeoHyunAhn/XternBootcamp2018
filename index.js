1dd192ed04f0e3b705de492b64d33c2ee6da8acc
SeoHyun Ahn <ahn67@purdue.edu>
2018-05-15 10:42:06 -0400

    chage during class - refresh input, auto focus on input, concat string

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 //console.log('ot works');
 //hw: make button change text in the heading
-function changeh1(){
+function changeh1() {
   const header = document.querySelector('h1')
   header.textContent = "changed"
 }
 // add multiple heading to the page and make the button change the second one
-function changeh2(){
+function changeh2() {
   const header = document.querySelector('h2')
   header.textContent = "Changing the second heading"
 }
 
 //add a form to the page, text input in form, update the heading with the text typeed
-function changeTitle(ev){
+function changeTitle(ev) {
   const header = document.getElementById('input').value
   // debugger
   document.getElementById('h1').innerHTML = header
 }
 
 const form = document.querySelector('form#userForm')
-const handlesubmit = function(ev){
+const handlesubmit = function(ev) {
   ev.preventDefault()
-  const heading = document.querySelector('h1')
+  const users = document.querySelector('#users')
   // debugger
   // heading.textContent=document.querySelector('input').value
-  heading.textContent=ev.target.userName.value
+  users.textContent += ' '+ev.target.userName.value
+  form.userName.value = ''
 }
 form.addEventListener('submit', handlesubmit)
