c47950afdc95965f5b45918cdf0522193e45509d
SeoHyun Ahn <ahn67@purdue.edu>
2018-05-16 09:44:45 -0400

    change during class - Extract renderListItem function

--- index.js
+++ index.js
@@ -15,26 +15,31 @@ function renderList(users){
   const age = form.age.value
   const color = form.favoriteColor.value
 
-  renderListIterm(ul, name)
-  renderListIterm(ul, age)
-  renderListIterm(ul, color)
+  // renderListIterm(ul, name)
+  // renderListIterm(ul, age)
+  // renderListIterm(ul, color)
+  ul.appendChild(renderListIterm('Name: ', name));
+  ul.appendChild(renderListIterm('Age: ', age));
+  ul.appendChild(renderListIterm('Color: ', color));
   users.appendChild(ul)
 }
 
 function renderListIterm(ul, items) {
-  const list = ["Name: ", "Age: ", "Color: "]
-  let flag = 0;
+  // const list = ["Name: ", "Age: ", "Color: "]
+  // let flag = 0;
   const li = document.createElement('li')
-  li.textContent = `${list[index]} ${items}`
-  if (list[index] == "Color: ") {
-    li.appendChild(renderColor(items))
-    flag = 1;
-  }
-  ul.appendChild(li)
-  index += 1;
-  if (flag == 1) {
-    index = 0;
-  }
+  li.textContent = `${ul} ${items}`
+  return li;
+  // li.textContent = `${list[index]} ${items}`
+  // if (list[index] == "Color: ") {
+  //   li.appendChild(renderColor(items))
+  //   flag = 1;
+  // }
+  // ul.appendChild(li)
+  // index += 1;
+  // if (flag == 1) {
+  //   index = 0;
+  // }
 }
 
 function renderColor(color) {
